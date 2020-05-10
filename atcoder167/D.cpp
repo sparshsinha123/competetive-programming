3837201aa8a4a888ab9d17968eba5f0de39d755c
Sparsh5008 <sparsh5008@gmail.com>
2020-05-10 19:07:44 +0530

    atcoder rounds

---
+++ atcoder167/D.cpp
@@ -0,0 +1,95 @@
+#pragma GCC optimize("Ofast")
+#include "bits/stdc++.h"
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define IOS { ios :: sync_with_stdio(false); cin.tie(0); cout.tie(0); }
+#define ll long long
+#define umapii unordered_map <int,int>
+#define umapll unordered_map<ll,ll>
+#define mapll map<ll,ll>
+#define mapii map<int,int>
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+#define vs vector<string>
+#define vi vector<int>
+#define vll vector<ll>
+#define vpi vector<pi>
+#define vpll vector<pll>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define endl '\n'
+#define all(v) (v).begin(),(v).end()
+#define FOR(i,a,b) for(int i = a; i <= b; i++)
+#define MP(a,b) make_pair(a,b)
+#define debug(x)       cerr<< #x << " : " << x << endl;
+#define debuga(A,N)    cerr<< #A <<" : [";for(int i = 0; i<N;i++) cerr<<A[i]<<" "; cerr<<"]\n";
+#define debuga2(A,N,M) cerr<< #A << " : \n"; for(int i=0;i<N;i++){cerr<<"[";for(int j=0;j<M;++j) cerr<<A[i][j]<<" ";cerr<<"]\n";}
+#define debugp(p)      cerr<< #p <<" : "<<"("<<(p).first<<","<<(p).second<<")\n";
+#define debugv(v)      cerr<< #v <<" : "<<"[";for(int i = 0; i< (v).size(); i++) cerr<<v[i]<<" "; cerr<<"]\n";
+#define debugv2(v)     cerr<< #v << " : \n"; for(int i=0;i<v.size();i++){cerr<<"[";for(int j=0;j<(v[i].size());++j) cerr<<v[i][j]<<" ";cerr<<"]\n";}
+#define debugs(m)      cerr<< #m <<" : [ "; for(auto itr = m.begin(); itr!=m.end();itr++) cerr<<*itr<<" "; cerr<<"]\n";
+#define debugm(m)      cerr<< #m <<" : [ "; for(auto itr = m.begin();itr!=m.end(); itr++) cerr<<"("<<itr->first<<","<<itr->second<<") ";cerr<<"]\n";
+
+/* ordered set: 
+tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>
+member functions :
+1. order_of_key(k) : number of elements strictly lesser than k
+2. find_by_order(k) : k-th element in the set
+*/
+ll modexp(ll x, ll ex , ll md) {
+    ll ans = 1ll;
+    while (ex > 0) {
+        if (ex & 1ll) ans = (ans * x) % md;
+        ex >>= 1ll;
+        x = (x * x) % md;
+    }
+    return ans;
+}
+
+const int inf = 1e9 + 7;
+int main(){
+    IOS;
+    int n;
+    ll k;
+    cin >> n >> k;
+    vector<bool> vis(n + 1 , 0);
+    vector<ll> dis(n + 1, -1);
+    vector<int> a(n + 1);
+    for(int i = 1; i <= n; i++){
+        cin >> a[i];
+    }
+    int s = 1;
+    vis[s] = 1;
+    int nx = -1;
+    dis[s] = 0;
+    while(k != 0){
+        nx = a[s];
+        if(vis[nx]) break;
+        vis[nx] = 1;
+        dis[nx] = 1 + dis[s];
+        --k;
+        s = nx;
+    }
+    if(k == 0){
+        cout << s << endl;
+        return 0;
+    } 
+    int  len = (dis[s] - dis[nx] + 1);
+    k--;
+    s = nx;
+    int move = (k % len);
+    for(int i = 0; i < move; i++){
+        s = a[s];
+    }
+    cout << s << '\n';
+    #ifdef LOCAL
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";       
+    #endif
+    // g++ -DLOCAL -std=c++17 f.cpp
+}
